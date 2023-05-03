b1a1e354ab8e8d165c1a4ca80084305c3730da70
sanari2019 <samachoo2010@icloud.com>
2023-05-03 01:35:32 +0100

    new-commit

---
+++ MyPortfolioApp/forms.py
@@ -0,0 +1,34 @@
+import re
+from django import forms
+
+from MyPortfolioApp.filter import PortfolioFilter
+from .models import *
+
+
+class PortfolioForm(forms.ModelForm):
+
+    class Meta:
+      model=Portfolio
+      fields="__all__"
+
+
+class CoursesForm(forms.ModelForm):
+    class Meta:
+      model=Courses
+      fields="__all__"
+
+class SkillForm(forms.ModelForm):
+    class Meta:
+      model=Skill
+      fields="__all__"
+
+
+# class PortfolioForm(forms.ModelForm):
+#     class Meta:
+#       model=Portfolio
+#       fields="__all__"
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Files
+        fields = "__all__"
