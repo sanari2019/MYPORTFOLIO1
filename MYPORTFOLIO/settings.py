15df1058ee429574d7612c81d058ed9b9dc59510
sanari2019 <samachoo2010@icloud.com>
2023-05-04 12:28:02 +0100

    next-commit

--- MYPORTFOLIO/settings.py
+++ MYPORTFOLIO/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z&qp^r-u^rdz_c*std&r971#+nqn(pder^qu!2*!gzv&&2op%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
